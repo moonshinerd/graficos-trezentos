5af3c8f8c91f2d8e8c5eeb9be0a54ed6a722b7c4
Víctor Schmidt <121871105+moonshinerd@users.noreply.github.com>
2024-07-11 22:41:55 -0300

    Update gerando_grafico_media_turma.py

--- gerador_bob_esponja/gerando_grafico_media_turma.py
+++ gerador_bob_esponja/gerando_grafico_media_turma.py
@@ -5,10 +5,26 @@ import os
 current_dir = os.path.dirname(os.path.abspath(__file__))
 media = gf.media_turma
 if media < 0.5: media = 0.5
+if 0 <= media < 2:
+  cor='#e8e8e8'
+elif 2 <= media < 4:
+  cor='#ef5eeb'
+elif 4 <= media < 6:
+  cor='#ef9c8e'
+elif 6 <= media < 8:
+  cor='#0ea84c'
+elif 8 <= media < 10:
+  cor='#f4ea6e'
+elif 10 <= media < 12:
+  cor='#a57449'
+elif 12 <= media < 14:
+  cor='#56d19f'
+else:
+  cor='#62b3ef' 
 fig1 = go.Figure(go.Bar(
     x=['Média da Turma'],
     y=[media],
-    marker=dict(color=gf.cores,cornerradius=30),
+    marker=dict(color=cor,cornerradius=30),
     width=0.2,
 ))
 
