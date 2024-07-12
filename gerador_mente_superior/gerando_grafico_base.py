6e2d4df11288808dd85c0d407c385880fe1c0088
moonshinerd <vitorhls15@gmail.com>
2024-07-12 09:03:20 -0300

    adicionando novas features

--- gerador_mente_superior/gerando_grafico_base.py
+++ gerador_mente_superior/gerando_grafico_base.py
@@ -14,7 +14,7 @@ linha_grade = 1.3
 linha_grade1 = 1.1
 media_turma = media_y_data = df.iloc[:, 1].mean()
 dias_totais = 15
-dia_atual = 1
+dia_atual = 4
 porcent_graf_dias = (dia_atual / dias_totais) * 30
 
 # Extrair os dados para os eixos x e y
